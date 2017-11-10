be344a53a7ad3256820adb44143d7fabeb0b25db
Michael <mzhu105@gmail.com>
2017-11-10 12:43:44 -0800

    Redo array algorithms practice

--- array/lib/dynamic_array.rb
+++ array/lib/dynamic_array.rb
@@ -4,23 +4,32 @@ class DynamicArray
   attr_reader :length
 
   def initialize
+    @store = StaticArray.new(0)
+    @length = 0
   end
 
   # O(1)
   def [](index)
+    check_index(index)
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
+    @store[index] = value
   end
 
   # O(1)
   def pop
+    @length -= 1
+
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    @length += 1
+
   end
 
   # O(n): has to shift over all the elements.
@@ -36,6 +45,9 @@ class DynamicArray
   attr_writer :length
 
   def check_index(index)
+    if index >= @length
+      raise "index out of bounds"
+    end
   end
 
   # O(n): has to copy over all the elements to the new store.
