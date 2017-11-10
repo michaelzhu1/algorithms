c4e52999a7161221c0538f24d2216cc2540ccded
Michael <mzhu105@gmail.com>
2017-11-10 15:25:08 -0800

    Start ring buffer

--- array/lib/ring_buffer.rb
+++ array/lib/ring_buffer.rb
@@ -4,30 +4,55 @@ class RingBuffer
   attr_reader :length
 
   def initialize
+    @store = StaticArray.new(0)
+    @length = 0
+    @capacity = 8
+    @start_idx = 0
   end
 
   # O(1)
   def [](index)
+    check_index(index)
+    @store[(@start_idx + index) % @capacity]
   end
 
   # O(1)
   def []=(index, val)
+    check_index(index)
+    @store[(@start_idx + index) % @capacity] = val
   end
 
   # O(1)
   def pop
+    unless @length >= 0
+      raise "index out of bounds"
+    end
+    val = @store[@length - 1]
+    @length -= 1
+    val
   end
 
   # O(1) ammortized
   def push(val)
+    @length += 1
+    resize! if @length == @capacity
+    @store[@length - 1] = val
   end
 
   # O(1)
   def shift
+    raise "index out of bounds" if @length == 0
+    val = @store[0]
+    @store[0] = nil
+    @start_idx = (@start_idx + 1) % @capacity
+    @length -= 1
+    val
   end
 
   # O(1) ammortized
   def unshift(val)
+    @length += 1
+    @start_idx -= 1
   end
 
   protected
@@ -35,8 +60,12 @@ class RingBuffer
   attr_writer :length
 
   def check_index(index)
+    unless index >= 0 && index < @length
+      raise "index out of bounds"
+    end
   end
 
   def resize!
+    @capacity = @capacity * 2
   end
 end
