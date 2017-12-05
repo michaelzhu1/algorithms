2eda5c3720fb2f1037f59d49285333652232c884
Michael <mzhu105@gmail.com>
2017-12-04 18:43:56 -0800

    Finish maxchar

--- algo-casts/exercises/maxchar/index.js
+++ algo-casts/exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const count = {};
+  let max = 0;
+  let maxCharacter = '';
+  str.split("").forEach((char) => {
+    if (!count[char]) {
+      count[char] = 1;
+    } else {
+      count[char] += 1;
+    }
+  });
+  Object.keys(count).forEach((key) => {
+    if (count[key] > max) {
+      max = count[key];
+      maxCharacter = key;
+    }
+  });
+  return maxCharacter;
+}
 
 module.exports = maxChar;
