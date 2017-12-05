57f07576546858df6e73fd0ad0a1f7cbd5a1ca00
Michael <mzhu105@gmail.com>
2017-12-05 15:51:40 -0800

    Solve chunk

--- algo-casts/exercises/chunk/index.js
+++ algo-casts/exercises/chunk/index.js
@@ -8,6 +8,24 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+
+}
+
+// my own solution
+// function chunk(array, size) {
+//   const res = [];
+//   let subarray = [];
+//   array.forEach(el => {
+//     if (subarray.length < size) {
+//       subarray.push(el);
+//     } else {
+//       res.push(subarray);
+//       subarray = [el];
+//     }
+//   });
+//   res.push(subarray);
+//   return res;
+// }
 
 module.exports = chunk;
