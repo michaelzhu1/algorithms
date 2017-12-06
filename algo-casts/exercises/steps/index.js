d46e2097d39bc2966fec37e480e4ef76a4536415
Michael <mzhu105@gmail.com>
2017-12-06 15:51:07 -0800

    Add recursive solution

--- algo-casts/exercises/steps/index.js
+++ algo-casts/exercises/steps/index.js
@@ -17,18 +17,21 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for (let row = 0; row < n; row++) {
-    let stair = '';
-    for (let col = 0; col < n; col++) {
-      if (col <= row) {
-        stair += '#';
-      } else {
-        stair += ' ';
-      }
-    }
-    console.log(stair);
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
   }
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+  steps(n, row, stair);
 }
 
 module.exports = steps;
@@ -47,5 +50,19 @@ module.exports = steps;
 // function steps(n) {
 //   for (let i = 1; i <= n; i++) {
 //     console.log(Array(i).fill('#').join('') + Array(n - i).fill(' ').join(''));
+//   }
+// }
+
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+//     for (let col = 0; col < n; col++) {
+//       if (col <= row) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//     console.log(stair);
 //   }
 // }
