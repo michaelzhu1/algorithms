352a7afab65eb94fff4002042b39cb98ec103572
Michael <mzhu105@gmail.com>
2017-12-06 16:28:49 -0800

    Add recursive solution

--- algo-casts/exercises/pyramid/index.js
+++ algo-casts/exercises/pyramid/index.js
@@ -14,19 +14,38 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  const midpoint = Math.floor((n * 2 - 1) / 2);
-  for (let row = 0; row < n; row++) {
-    let stair = '';
-    for (let col = 0; col < 2*n-1; col++) {
-      if (col >= midpoint - row && col <= midpoint + row) {
-        stair += "#";
-      } else {
-        stair += ' ';
-      }
-    }
-    console.log(stair);
+function pyramid(n, row = 0, stair = '') {
+  let midpoint = Math.floor((n * 2 - 1) / 2);
+  if (n === row) {
+    return;
   }
+  if (stair.length === n * 2 - 1) {
+    console.log(stair);
+    return pyramid(n, row + 1);
+  }
+  if (stair.length >= (midpoint - row) && stair.length <= (midpoint + row)) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+  pyramid(n, row, stair);
 }
 
 module.exports = pyramid;
+
+
+
+// function pyramid(n) {
+//   const midpoint = Math.floor((n * 2 - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+//     for (let col = 0; col < 2*n-1; col++) {
+//       if (col >= midpoint - row && col <= midpoint + row) {
+//         stair += "#";
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
