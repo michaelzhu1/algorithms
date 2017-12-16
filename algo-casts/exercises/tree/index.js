81911e4d1728718574b960dc7abdcc46db16fc12
Michael <mzhu105@gmail.com>
2017-12-15 17:08:31 -0800

    Finish tree node implementation

--- algo-casts/exercises/tree/index.js
+++ algo-casts/exercises/tree/index.js
@@ -10,8 +10,34 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
 
-class Tree {}
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    this.children = this.children.filter(ele => {
+      return ele.data !== data;
+    });
+  }
+}
+
+class Tree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  traverseBF(fn) {
+    const queue = [this.root];
+    while (queue.length > 0) {
+      queue.concat(queue[0].children);
+    }
+  }
+}
 
 module.exports = { Tree, Node };
