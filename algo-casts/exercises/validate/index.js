39a3f65031ce983237bcd079781cbdd521203e1e
Michael <mzhu105@gmail.com>
2017-12-18 16:07:53 -0800

    Complete validating binary search tree

--- algo-casts/exercises/validate/index.js
+++ algo-casts/exercises/validate/index.js
@@ -5,6 +5,20 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+// initial attempt solution, may not be optimized
+function validate(node, min = null, max = null) {
+  if (max && node.data > max) {
+    return false;
+  } else if (min && node.data < min) {
+    return false;
+  }
+  if (node.left) {
+    return validate(node.left, min, node.data);
+  } else if (node.right) {
+    return validate(node.right, node.data, max);
+  } else {
+    return true;
+  }
+}
 
 module.exports = validate;
