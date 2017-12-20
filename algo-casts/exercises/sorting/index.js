bbe9091aaa734f7274d5899a41c1fe9a1d4b5101
Michael <mzhu105@gmail.com>
2017-12-20 10:40:08 -0800

    Implement my own version of bubbleSort

--- algo-casts/exercises/sorting/index.js
+++ algo-casts/exercises/sorting/index.js
@@ -2,7 +2,19 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  let sorted = false;
+  while (!sorted) {
+    sorted = true;
+    for (let j = 0; j < (arr.length - 1); j++) {
+      if (arr[j] > arr[j + 1]) {
+        sorted = false;
+        const lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+  return arr;
 }
 
 function selectionSort(arr) {
