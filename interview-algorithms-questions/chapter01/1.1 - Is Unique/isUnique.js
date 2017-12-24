53d44f752e3d15786004d06d853d01f16a0f8005
Michael <mzhu105@gmail.com>
2017-12-23 21:59:30 -0800

    Add isUnique

--- interview-algorithms-questions/chapter01/1.1 - Is Unique/isUnique.js
+++ interview-algorithms-questions/chapter01/1.1 - Is Unique/isUnique.js
@@ -1,7 +1,19 @@
-var allUniqueChars = function(string) {};
+function allUniqueChars(string) {
+  const seen = {};
+  for (let i = 0; i < string.length; i++) {
+    if (seen[string[i]]) {
+      return false;
+    } else {
+      seen[string[i]] = true;
+    }
+  }
+  return true;
+}
 
 /* TESTS */
 // log some tests here
 
 console.log(allUniqueChars('bkjaewf'));
 console.log(allUniqueChars('bkjaewfb'));
+console.log(allUniqueChars('abcdefg'));
+console.log(allUniqueChars('abcdefgac'));
