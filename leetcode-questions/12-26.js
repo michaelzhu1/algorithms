c563d193ba79d5e66b6492b42b2b1f4999f1b650
Michael <mzhu105@gmail.com>
2017-12-26 15:46:04 -0800

    Finish reverseLinkedList

---
+++ leetcode-questions/12-26.js
@@ -0,0 +1,49 @@
+// 175. Combine Two Tables
+// Table: Person
+//
+// +-------------+---------+
+// | Column Name | Type    |
+// +-------------+---------+
+// | PersonId    | int     |
+// | FirstName   | varchar |
+// | LastName    | varchar |
+// +-------------+---------+
+// PersonId is the primary key column for this table.
+// Table: Address
+//
+// +-------------+---------+
+// | Column Name | Type    |
+// +-------------+---------+
+// | AddressId   | int     |
+// | PersonId    | int     |
+// | City        | varchar |
+// | State       | varchar |
+// +-------------+---------+
+// AddressId is the primary key column for this table.
+//
+// Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:
+//
+// FirstName, LastName, City, State
+
+// SELECT
+// FirstName, LastName, City, State
+// FROM
+// Person
+// LEFT JOIN
+// Address
+// ON
+// Person.PersonId = Address.PersonId;
+
+
+// 206. Reverse Linked List
+var reverseList = function(head) {
+    let prev = null;
+    let curr = head;
+    while (curr !== null) {
+        let tempNext = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = tempNext;
+    }
+    return prev;
+};
