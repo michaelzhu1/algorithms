7b6d23513556515e182d86212d427fb357f5f2de
Michael <mzhu105@gmail.com>
2017-12-25 17:36:23 -0800

    Finish moveZeros

--- leetcode-questions/12-25.js
+++ leetcode-questions/12-25.js
@@ -1,12 +1,26 @@
 var isPalindrome = function(s) {
-    //轉小寫
     s = s.toLowerCase();
-    //取代非文字部分
     s = s.replace(/[^a-z0-9]/ig,"");
-    //反轉
     var rev = s.split("").reverse().join("");
-    //判斷反轉後是否與之前處理過的字串相等
     return s.indexOf(rev) == 0;
 };
 
 console.log(isPalindrome('abbubba'));
+
+// 283. Move Zeroes
+// Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+//
+// For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+var moveZeroes = function(nums) {
+    let index = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[index] = nums[i];
+            index++;
+        }
+    }
+    for (index; index < nums.length; index++) {
+        nums[index] = 0;
+    }
+};
