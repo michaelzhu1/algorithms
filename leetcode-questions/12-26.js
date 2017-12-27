012bf85d62df228f23d14efb3df2949f66da0a7f
Michael <mzhu105@gmail.com>
2017-12-26 17:10:40 -0800

    Finished merge sorted list

--- leetcode-questions/12-26.js
+++ leetcode-questions/12-26.js
@@ -116,3 +116,12 @@ var deleteDuplicates = function(head) {
     }
     return head;
 };
+
+// 88. Merge Sorted Array
+// Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+var merge = function(nums1, m, nums2, n) {
+    for (let i = m; i < nums1.length; i++) {
+        nums1[i] = nums2.shift();
+    }
+    nums1.sort((a,b) => a-b);
+};
