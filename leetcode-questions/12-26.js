c5bf70ae2107e557743564205e1d5e3a7b995fbe
Michael <mzhu105@gmail.com>
2017-12-26 17:00:33 -0800

    Finish deleteDuplicates

--- leetcode-questions/12-26.js
+++ leetcode-questions/12-26.js
@@ -97,3 +97,22 @@ var plusOne = function(digits) {
     }
     return digits;
 };
+
+// 83. Remove Duplicates from Sorted List
+// Given a sorted linked list, delete all duplicates such that each element appear only once.
+//
+// For example,
+// Given 1->1->2, return 1->2.
+// Given 1->1->2->3->3, return 1->2->3.
+
+var deleteDuplicates = function(head) {
+    let curr = head;
+    while (curr !== null && curr.next !== null) {
+        if (curr.val === curr.next.val) {
+            curr.next = curr.next.next;
+        } else {
+            curr = curr.next;
+        }
+    }
+    return head;
+};
