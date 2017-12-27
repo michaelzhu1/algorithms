769aefe77cccf7c13866f32d595fe2b218a2ff50
Michael <mzhu105@gmail.com>
2017-12-26 16:33:06 -0800

    Finish plusOne

--- leetcode-questions/12-26.js
+++ leetcode-questions/12-26.js
@@ -65,3 +65,35 @@ var isPalindrome = function(head) {
 };
 
 // The trick is to convert the linked list to strings and then compare if the reverse of that string is the same as the original string.
+
+// 66. Plus One
+// Given a non-negative integer represented as a non-empty array of digits, plus one to the integer.
+//
+// You may assume the integer do not contain any leading zero, except the number 0 itself.
+//
+// The digits are stored such that the most significant digit is at the head of the list.
+var plusOne = function(digits) {
+    if (digits[digits.length - 1] !== 9) {
+        digits[digits.length - 1]++;
+        return digits;
+    } else {
+        let addOne = true;
+        let i = digits.length - 1;
+        while (addOne) {
+            if (digits[i] === 9) {
+                digits[i] = 0;
+                if (digits[i-1] && digits[i-1] === 9) {
+                    addOne = true;
+                } else if (!digits[i-1]) {
+                    digits.unshift(1);
+                    addOne = false;
+                }
+                i--;
+            } else {
+                digits[i]++;
+                addOne = false;
+            }
+        }
+    }
+    return digits;
+};
