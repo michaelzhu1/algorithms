bbce52e0a7442958b6591610c4fa7e570d42f862
Michael <mzhu105@gmail.com>
2017-12-26 23:37:07 -0800

    Finish levelOrder for binary tree

--- leetcode-questions/12-26.js
+++ leetcode-questions/12-26.js
@@ -193,3 +193,48 @@ var invertTree = function(root) {
     }
     return root;
 };
+
+// 102. Binary Tree Level Order Traversal
+// Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+//
+// For example:
+// Given binary tree [3,9,20,null,null,15,7],
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// return its level order traversal as:
+// [
+//   [3],
+//   [9,20],
+//   [15,7]
+// ]
+
+var levelOrder = function(root) {
+    const queue = [root];
+    const result = [];
+    if (root === null) {
+        return result;
+    }
+    while (queue.length > 0) {
+        const levelSize = queue.length;
+        let currLevel = [];
+        for (let i = 0; i < levelSize; i++) {
+            const node = queue.shift();
+            if (node.left !== null) {
+              queue.push(node.left);
+            }
+            if (node.right !== null) {
+              queue.push(node.right);
+            }
+            currLevel.push(node.val);
+        }
+        if (currLevel.length !== 0) {
+            result.push(currLevel);
+        }
+    }
+    return result;
+};
+
+// Finally figured it out with BFS
