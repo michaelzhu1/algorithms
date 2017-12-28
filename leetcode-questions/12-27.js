426eefb952e87ed716d1abe36a5514f320575d2b
Michael <mzhu105@gmail.com>
2017-12-27 17:11:34 -0800

    Finish both minBinaryTree and maxBinaryTree

--- leetcode-questions/12-27.js
+++ leetcode-questions/12-27.js
@@ -70,3 +70,35 @@ var minSubArrayLen = function(s, nums) {
 };
 
 //  Used a left pointer to keep track the left most index while keep shrinking the length to see if the sum is still more than s. Used a ans variable to keep track the minimum length, and preset it to Number.MAX_SAFE_INTEGER
+//
+// 104. Maximum Depth of Binary Tree
+// Given a binary tree, find its maximum depth.
+//
+// The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+var maxDepth = function(root) {
+    if (root === null) {
+        return 0;
+    }
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
+
+// 111. Minimum Depth of Binary Tree
+// Given a binary tree, find its minimum depth.
+//
+// The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+var minDepth = function(root) {
+    if (root === null) {
+        return 0;
+    }
+    if (root.left === null && root.right === null) {
+        return 1;
+    } else if (root.left === null) {
+        return minDepth(root.right) + 1;
+    } else if (root.right === null) {
+        return minDepth(root.left) + 1;
+    } else {
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+};
