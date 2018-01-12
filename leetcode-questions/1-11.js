26a5bb15c058a808fd2c0450d7d94c947581ba23
Michael <mzhu105@gmail.com>
2018-01-11 16:33:59 -0800

    containsNearbyAlmostDuplicate

--- leetcode-questions/1-11.js
+++ leetcode-questions/1-11.js
@@ -55,3 +55,17 @@ var containsNearbyDuplicate = function(nums, k) {
     }
     return false;
 };
+
+
+// 220. Contains Duplicate III
+// Given an array of integers, find out whether there are two distinct indices i and j in the array such that the absolute difference between nums[i] and nums[j] is at most t and the absolute difference between i and j is at most k.
+var containsNearbyAlmostDuplicate = function(nums, k, t) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if ((Math.abs(nums[i] - nums[j]) <= t) && (Math.abs(i - j) <= k)) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
