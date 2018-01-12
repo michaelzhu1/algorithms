4583f1bf346591fe84ac2a6a5036a5daf1f53411
Michael <mzhu105@gmail.com>
2018-01-11 16:20:07 -0800

    containsNearbyDuplicate

--- leetcode-questions/1-11.js
+++ leetcode-questions/1-11.js
@@ -41,3 +41,17 @@ var lengthOfLongestSubstring = function(s) {
     }
     return result;
 };
+
+//
+// 219. Contains Duplicate II
+// Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+var containsNearbyDuplicate = function(nums, k) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j] && Math.abs(i - j) <= k) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
